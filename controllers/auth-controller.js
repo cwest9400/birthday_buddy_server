9fe60f8d57b4040a913cd01aa771eab131993b5f
cwest9400 <chrstphrwest@gmail.com>
2023-01-31 22:03:00 -0500

    added register route and loggin route

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,10 +1,32 @@
 const express = require('express');
 const router = express.Router();
-
+const bodyParser =require("body-parser")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
 const { User } = require('../models');
+
 
 router.use(express.json());
 
 
+
 //creating an account
-router.post("")
+router.post("/register", async (req, res) => {
+    const user = req.body
+    
+    const takenEmail = await User.findOne({email: user.email})
+    if (takenEmail) {
+        res.json({message: "There is already an account registered with that Email."})
+    } else {
+        user.password = await bcrypt.hash(req.body.password, 10)
+
+        const dbUser = new User( {
+            firstName: user.firstName.toLowerCase(),
+            lastName: user.lastName.toLowerCase(),
+            email: user.email.toLowerCase(),
+            password: user.password
+        })
+        dbUser.save()
+        res.json({message: "Success"})
+    }
+})
--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const bodyParser =require("body-parser")
+
+//auth stuff
+const bodyParser = require("body-parser")
 const jwt = require("jsonwebtoken")
 const bcrypt = require("bcrypt")
 const User = require("./models/User")
-
+///////
 const app = express();
 
 //body parser middleware
-const urlencodedParser = bodyParser.urlencoded({ extended: false})
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(bodyParser.json(), urlencodedParser)
 
 //require controllers//
 const birthdaysController = require('./controllers/birthdays-controller')
+const authController = require('./controllers/auth-controller')
 //////////////
 
 require('dotenv').config();
@@ -27,14 +30,78 @@ app.use(morgan('dev'))
 
 //app.us(/'?', ?Controller) controller
 app.use('/birthdays', birthdaysController)
+// app.use('/register', authController)
 
 
 //////////////
 
 //home redirect
-app.get('/', (req,res)=>res.redirect('/birthdays'))
+app.get('/', (req, res) => res.redirect('/birthdays'))
 
-app.listen(PORT, ()=> {
-    console.log(`listening on: ${PORT}`)
+
+///auth route///
+//creating an account
+app.post("/register", async (req, res) => {
+    const user = req.body
+
+    const takenEmail = await User.findOne({ email: user.email })
+    if (takenEmail) {
+        res.json({ message: "There is already an account registered with that Email." })
+    } else {
+        user.password = await bcrypt.hash(req.body.password, 10)
+
+        const dbUser = new User({
+            firstName: user.firstName.toLowerCase(),
+            lastName: user.lastName.toLowerCase(),
+            email: user.email.toLowerCase(),
+            password: user.password
+        })
+        dbUser.save()
+        res.json({ message: "Success" })
+    }
 })
+
+//LOGIN
+app.post("/login", (req, res) => {
+    const userLoggingIn = req.body;
+
+    User.findOne({ email: userLoggingIn.email })
+        .then(dbUser => {
+            if (!dbUser) {
+                return res.json({
+                    message: "Wrong Email or Password"
+                })
+            }
+            bcrypt.compare(userLoggingIn.password, dbUser.password)
+                .then(isCorrect => {
+                    if (isCorrect) {
+                        const payload = {
+                            id: dbUser._id,
+                            email: dbUser.email,
+                        }
+                        jwt.sign(
+                            payload,
+                            process.env.JWT_SECRET,
+                            { expiresIn: 86400 },
+                            (err, token) => {
+                                if (err) return res.json({ message: err })
+                                return res.json({
+                                    message: "Success",
+                                    token: "Bearer " + token
+                                })
+                            }
+                        )
+                    } else {
+                        return res.json({
+                            message: "Wrong Email or Password"
+                        })
+                    }
+                })
+        })
+})
+
+    /////////////////////////////////////////////
+    app.listen(PORT, () => {
+        console.log(`listening on: ${PORT}`)
+    })
 
